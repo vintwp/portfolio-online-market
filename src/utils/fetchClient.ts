d3800b9d24a5227aa7cf5585dd2cd32f03a1fd20
VitaliiPa <poroh1991ukr@gmail.com>
2024-05-05 13:40:04 +0300

    fixed img urls for product sliders

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -3,7 +3,6 @@ import { Product } from '../types/Product';
 import { ProductDetail } from '../types/ProductDetail';
 import { ProductCategories } from '../types/ProductCategories';
 
-const REACT_APP_BASE_URL = import.meta.env.REACT_APP_BASE_URL || '';
 const API_PRODUCT_LIST = import.meta.env.VITE_API_PRODUCT_LIST_ALL || '';
 const API_PRODUCT_LIST_BY_CATEGORY =
   import.meta.env.VITE_API_PRODUCT_LIST_BY_CATEGORY || '';
@@ -24,17 +23,6 @@ export function getFromServer<T>(url: string): Promise<T> {
   });
 }
 
-function makePathForImages<T extends { image?: string }>(data: T[]) {
-  const dataProducts = [...data].map(product => {
-    return {
-      ...product,
-      image: `${REACT_APP_BASE_URL}/${product.image}`,
-    };
-  });
-
-  return dataProducts;
-}
-
 export async function getAllProducts() {
   try {
     const products = await getFromServer<Product[]>(API_PRODUCT_LIST);
@@ -46,16 +34,13 @@ export async function getAllProducts() {
 }
 
 export function getBrandNewProducts(products: Product[]) {
-  const productsWithImagePath = makePathForImages(products);
   const latestYear = Math.max(...products.map(product => product.year));
 
-  return productsWithImagePath.filter(product => product.year === latestYear);
+  return products.filter(product => product.year === latestYear);
 }
 
 export function getHotPriceProducts(products: Product[]) {
-  const productsWithImagePath = makePathForImages(products);
-
-  return productsWithImagePath.sort((product1, product2) => {
+  return products.sort((product1, product2) => {
     return (
       product2.fullPrice -
       product2.price -
